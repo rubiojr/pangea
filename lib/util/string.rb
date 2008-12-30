2cd805554707113493a0d31726df39b1e78dc225
Sergio Rubio <sergio@rubio.name>
2008-12-30 13:29:45 +0100

    * support tera an peta byte scales

--- lib/util/string.rb
+++ lib/util/string.rb
@@ -3,7 +3,7 @@ module Pangea
 
     def self.humanize_bytes(bytes)
       m = bytes.to_i
-      units = %w[Bits Bytes MB GB]
+      units = %w[Bits Bytes MB GB TB PB]
       while (m/1024.0) >= 1 
         m = m/1024.0
         units.shift
