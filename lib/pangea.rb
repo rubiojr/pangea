c8c30437987f0f6b47361ee8b4ba2d39f0ac69f1
Sergio Rubio <sergio@rubio.name>
2008-12-30 13:30:33 +0100

    * implemented more api methods

--- lib/pangea.rb
+++ lib/pangea.rb
@@ -29,7 +29,7 @@ module Pangea
       @links.each do |hl|
         ref = hl.client.call('host.get_all', hl.sid)['Value'][0]
         h = Host.new(hl, ref, hl.client.proxy('host'))
-        @index[h.name_label] = h
+        @index[h.label] = h
         list << h
       end
       list
@@ -102,7 +102,7 @@ module Pangea
       ref_call :get_uuid
     end
 
-    def name_label
+    def label
       ref_call :get_name_label
     end
 
@@ -119,7 +119,7 @@ module Pangea
     end
     
     memoize :metrics
-    memoize :name_label
+    memoize :label
     memoize :resident_vms
   end
 
@@ -145,11 +145,143 @@ module Pangea
       super link, ref, proxy
     end
 
-    def name_label
+    #
+    # xen-api: VM.get_name_label
+    #
+    def label
       ref_call :get_name_label
     end
+    
+    #
+    # xen-api: VM.get_metrics
+    #
+    def metrics
+      VMMetrics.new(@link, ref_call(:get_metrics), @link.client.proxy('VM_metrics'))
+    end
 
-    memoize :name_label
+    #
+    # xen-api: VM.get_power_state
+    # 
+    def power_state
+      ref_call :get_power_state
+    end
+
+    #
+    # xen-api: VM.get_memory_static_max
+    #
+    def max_mem
+      ref_call :get_memory_static_max
+    end
+    
+    def dyn_max_mem
+      ref_call :get_memory_dynamic_max
+    end
+
+    def dyn_min_mem
+      ref_call :get_memory_dynamic_min
+    end
+
+    #
+    # xen-api: VM.get_memory_static_min
+    #
+    def min_mem
+      ref_call :get_memory_static_min
+    end
+
+    #
+    # xen-api: VM.get_resident_on
+    #
+    #def resident_on
+    #  ref = ref_cal :get_power_state
+    #  Host.new(hl, ref, hl.client.proxy('host'))
+    #end
+    
+    def inspect
+      eol = "\n"
+      "[VM]" + eol +
+      "Label: #{label}" + eol +
+      "Power State: #{power_state}" + eol +
+      "Mem: #{Util::humanize_bytes(dyn_min_mem)}" + eol +
+      "Max Mem: #{Util::humanize_bytes(max_mem)}" + eol
+
+    end
+
+    memoize :label
+    memoize :metrics
   end 
+
+  #
+  # xen-api class: VM_metrics
+  #
+  class VMMetrics < XObject
+    
+    def initialize(link, ref, proxy)
+      super(link, ref, proxy)
+    end
+
+    #
+    # xen-api: VM_metrics.get_memory_actual
+    #
+    def memory_actual
+      ref_call :get_memory_actual
+    end
+
+    #
+    # xen-api: VM_metrics.get_VCPUs_number
+    # Number of cpus assigned to the domU
+    #
+    def vcpus_number
+      ref_call :get_VCPUs_number
+    end
+    
+    #
+    # xen-api: VM_metrics.get_VCPUs_utilisation
+    #
+    # returns a hash
+    # {
+    #   cpu0 => utilisation,
+    #   cpu1 => utilization,
+    #   ...
+    # }
+    #  
+    def vcpus_utilisation
+      ref_call :get_VCPUs_utilisation
+    end
+
+    #
+    # xen-api: VM_metrics.get_state
+    # 
+    def state
+      ref_call :get_state
+    end
+
+    #
+    # xen-api: VM_metrics.get_start_time
+    #
+    def start_time
+      (ref_call :get_start_time).to_time
+    end
+
+    #
+    # xen-api: VM_metrics.get_last_updated
+    #
+    def last_updated
+      (ref_call :get_last_updated).to_time
+    end
+
+    def inspect
+      vcpu_u = ""
+      vcpus_utilisation.each do |k, v|
+        vcpu_u += "#{k}: %0.2f\n" % (v * 100)
+      end
+      eol = "\n"
+      "[VM Metrics]" + eol +
+      "State: #{state}" + eol +
+      "Start Time: #{start_time}" + eol +
+      "Last Updated: #{last_updated}" + eol +
+      "VCPUs Utilisation:" + eol +
+      vcpu_u
+    end
+  end
 end # module Pangea
 
