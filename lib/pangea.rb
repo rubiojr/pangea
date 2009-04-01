720000e96adab4f06f8ac95d9e793c9ae8e1e0ce
Sergio Rubio <sergio@rubio.name>
2009-04-01 13:44:18 +0200

    * bumped version to 0.1

--- lib/pangea.rb
+++ lib/pangea.rb
@@ -4,6 +4,6 @@ require "#{File.join(File.dirname(__FILE__), 'pangea/objects.rb')}"
 require "#{File.join(File.dirname(__FILE__), 'pangea/exceptions.rb')}"
 
 module Pangea
-  VERSION = "0.0.20090313130744"
+  VERSION = "0.1.20090401134350"
 end # module Pangea
 
