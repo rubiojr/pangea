367e74cc118aa4a3b1ca6f1bd41b9c8ae4d39ba3
Sergio Rubio <sergio@rubio.name>
2009-04-03 13:44:31 +0200

    * new minor bugfix release 0.1.20090403134419 * removed debugging prings * updated docs * code cleanup

--- lib/pangea.rb
+++ lib/pangea.rb
@@ -4,6 +4,6 @@ require "#{File.join(File.dirname(__FILE__), 'pangea/objects.rb')}"
 require "#{File.join(File.dirname(__FILE__), 'pangea/exceptions.rb')}"
 
 module Pangea
-  VERSION = "0.1.20090401135815"
+  VERSION = "0.1.20090403134419"
 end # module Pangea
 
